341404eeb7321b7ad7c80fd91625906aac04be41
Edin Herić <84852928+hera-beast@users.noreply.github.com>
2021-05-28 19:16:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+var ball = document.getElementById('ball');
+
+addEventListener('keydown', move);
+
+function move(e) {
+	
+    if(e.which == 37){
+	ball.style.left = '50px';
+	}
+	
+	 if(e.which == 38){
+	ball.style.top = '50px';
+	}
+	
+	 if(e.which == 39){
+	ball.style.left = '1400px';
+	}
+	
+	 if(e.which == 40){
+	ball.style.top = '600px';
+	}
+
+
+}
+
